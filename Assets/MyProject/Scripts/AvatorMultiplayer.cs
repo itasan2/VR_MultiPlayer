d8b8489b9b19205702bceac8df6845e6b654fbd7
itasan2 <itasaka@sa2.so-net.ne.jp>
2021-04-17 01:32:46 +0900

    Version 0.1

---
+++ Assets/MyProject/Scripts/AvatorMultiplayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.XR;
+
+public class AvatorMultiplayer : NetworkBehaviour
+{
+	public override void OnStartLocalPlayer()
+	{
+		if(isServer) {
+			XRSettings.enabled = true;
+		}
+
+		GameObject camera = GameObject.FindWithTag("MainCamera");
+		transform.parent = camera.transform;
+		transform.localPosition = Vector3.zero;
+		transform.rotation = camera.transform.rotation;
+	}
+}
---
+++ Assets/MyProject/Scripts/NetworkStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class NetworkStart : MonoBehaviour
+{
+	void Awake()
+	{
+		XRSettings.enabled = false;
+	}
+}
